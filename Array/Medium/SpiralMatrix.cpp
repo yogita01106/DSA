a146cf9ffa09a7f1f1a67729c7ce0f598e9bd2fa
yogita01106 <yogitakharb01@gmail.com>
2026-06-24 17:37:11 +0000

    Print the element of Matrix in Spiral Manner

---
+++ Array/Medium/SpiralMatrix.cpp
@@ -0,0 +1,42 @@
+/* Problem Statement:Given an M * N matrix, print the elements in a clockwise spiral manner.
+Return an array with the elements in the order of their appearance when printed in a spiral manner.
+Example 1
+Input: matrix = [[1, 2, 3], [4 ,5 ,6], [7, 8, 9]]
+Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+Explanation:
+The elements in the spiral order are 1, 2, 3 -> 6, 9 -> 8, 7 -> 4, 5 */
+//Time Complexity-O(m*n)
+//Space Complexity -O(1)
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>result;
+        int top=0;
+        int bottom=matrix.size()-1;
+        int left=0;
+        int right=matrix[0].size()-1;
+        while(top<=bottom && left<=right){
+            for (int i=left;i<=right;i++){
+                result.push_back(matrix[top][i]);
+            }
+            top++;
+            for (int i=top;i<=bottom;i++){
+                result.push_back(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for (int i=right;i>=left;i--){
+                    result.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left<=right){
+                for (int i=bottom;i>=top;i--){
+                    result.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+};
